a6fdf2a6a4fecd6cc0f721d48198600906256594
BaDjinn <g.sottovia@yahoo.com>
2023-12-10 19:33:35 +0100

    .

--- routes/jsonRoutes.js
+++ routes/jsonRoutes.js
@@ -5,27 +5,11 @@ const dataPakage = {
 	message: "Hello json",
 };
 
-// json.get("/json", async (req, res) => {
-// 	try {
-// 		process.env.MESSAGE_STYLE == `uppercase`
-// 			? res.status(200).json(dataPakage.message.toUpperCase())
-// 			: res.status(200).json(dataPakage);
-// 		console.log("get /json for dataPakage");
-// 	} catch (error) {
-// 		res.status(404).send({
-// 			statusCode: 400,
-// 			error: error,
-// 		});
-// 	}
-// });
-
 json.get("/json", async (req, res) => {
 	try {
-		if (process.env["MESSAGE_STYLE"] === "uppercase") {
-			res.json(dataPakage.message.toUpperCase());
-		} else {
-			res.json(dataPakage);
-		}
+		process.env.MESSAGE_STYLE == `uppercase`
+			? res.status(200).json({ message: "HELLO JSON" })
+			: res.status(200).json({ message: "Hello json" });
 		console.log("get /json for dataPakage");
 	} catch (error) {
 		res.status(404).send({
@@ -34,5 +18,21 @@ json.get("/json", async (req, res) => {
 		});
 	}
 });
+
+// json.get("/json", async (req, res) => {
+// 	try {
+// 		if (process.env["MESSAGE_STYLE"] === "uppercase") {
+// 			res.json(dataPakage.message.toUpperCase());
+// 		} else {
+// 			res.json(dataPakage);
+// 		}
+// 		console.log("get /json for dataPakage");
+// 	} catch (error) {
+// 		res.status(404).send({
+// 			statusCode: 400,
+// 			error: error,
+// 		});
+// 	}
+// });
 
 module.exports = json;

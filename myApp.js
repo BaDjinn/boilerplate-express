2b64fc48e9f2de15c330b3d040f1b10ded558258
BaDjinn <g.sottovia@yahoo.com>
2023-12-10 18:15:40 +0100

    .

--- myApp.js
+++ myApp.js
@@ -1,10 +1,22 @@
-let express = require("express");
-let app = express();
+const express = require("express");
+const app = express();
 
 console.log("Hello World");
 
-app.get("/", (req, res) => {
-	res.sendFile(__dirname + `/views/index.html`);
+//middleware
+app.use("/public", express.static(__dirname + `/public`));
+
+//routing
+app.get("/", async (req, res) => {
+	try {
+		res.status(200).sendFile(__dirname + `/views/index.html`);
+		console.log(`get / for /views/index.html`);
+	} catch (error) {
+		res.status(404).send({
+			statusCode: 200,
+			error: error,
+		});
+	}
 });
 
 module.exports = app;
